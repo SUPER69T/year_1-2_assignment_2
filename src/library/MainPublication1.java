244b35e0f55ad4af3f8004e7703f1c2cf0ade58f
Leonsubb <Leonsubb@gmail.com>
2025-09-07 23:18:39 +0300

    Worm-food

---
+++ src/library/MainPublication1.java
@@ -0,0 +1,32 @@
+package library;
+
+public class MainPublication1 {
+
+    public static void main(String[] args) {
+        Publication[] publications = new Publication[100];
+        Author[] authors = new Author[100];
+        authors[0] = new Author("Harper Lee","harper@somwhere.com");
+        authors[1] = new Author("F. Scott Fitzgerald","scott@somwhere.com");
+        authors[2] = new Author("Amos Oz","oz@somwhere.com");
+        authors[3] = new Author("Giora Alexandron","giora@somwhere.com");
+        authors[4] = new Author("Avraham Aizenbud","avi@somwhere.com");
+        authors[5] = new Author("Ezra Hadad","ezra@somwhere.com");
+
+        publications[0]= new Book("To Kill a Mockingbird", "J. B. Lippincott & Co.",
+                2, authors[0],"Tay Hohoff", 1960);
+        publications[1]=new Journal("Scientific American", "Springer Nature", 5, 135, 2021);
+        publications[2]=new Encyclopedia("Encyclopedia Britannica", "Encyclopedia Britannica, Inc.", 1, "",1);
+        publications[3]=new Encyclopedia("Encyclopedia Britannica", "Encyclopedia Britannica, Inc.", 1, "",3);
+        publications[4]=new Book("The Great Gatsby", "Charles Scribner's Sons",
+                1, authors[1],"Maxwell Perkins", 1925);
+        publications[5]=new Journal("Teva Hadvarim","Hahevra le-Heker Haadam ve-Hasovev ltd",5,297,2020);
+        publications[6]=new Encyclopedia("Encyclopedia Americana", "Scholastic Corporation", 1, "", 30);
+        publications[7]=new Book("Sippur Al Ahava VeChoshech", "Houghton Mifflin Harcourt",
+                1, authors[2],"", 2002);
+        publications[8]=new Encyclopedia("Encyclopedia Britannica", "Encyclopedia Britannica, Inc.", 1, "",2);
+        publications[9]=new Journal("Nature","Springer Nature",
+                5,  590,2021 );
+
+
+    }
+}
---
+++ src/library/MainStudents.java
@@ -0,0 +1,17 @@
+/*
+package library;
+
+
+
+public class MainStudents {
+    public static void main(String[] args) {
+        Student[] students = new Student[100];
+        students[0]=new Student("Albert Einstein");
+        students[1]= new Student("Marie Curie");
+        students[2]= new Student("Rosalind Franklin");
+        students[3]= new Student("Stephen Hawking");
+        listStudents(students);
+    }
+
+}
+*/
