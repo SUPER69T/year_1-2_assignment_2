34fffcd881e13e06177c9df7d6d5d5b61963970e
Leonsubb <Leonsubb@gmail.com>
2025-09-07 21:11:35 +0300

    what

--- src/library/Publication.java
+++ src/library/Publication.java
@@ -3,5 +3,20 @@ public class Publication {
     private String title;
     private String publication;
     private int qty;
-    final static int MAX_QTY = 10;
+    final static int Pub_def_id = 10;
+    static private int def_id = Pub_def_id;
+    public Publication(String title, String publication, int qty){
+        this.title = title;
+        this.publication = publication;
+        this.qty = qty;
+        def_id++;
+    }
+    //@Override
+    public boolean equals(Publication old_pub){
+        return (this.title.equals(old_pub.title) && (this.publication.equals(old_pub.publication))) && (this.qty == old_pub.qty);
+    }
+    @Override
+    public String toString(){
+        return "Title: " + title + ", Publication: " + publication + ", Qty: " + qty;
+    }
 }
